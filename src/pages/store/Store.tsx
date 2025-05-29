1d4b82b772a0572a5f0bd055dcdceacf22f7130f
parmis <parmis.khorshidi@gmail.com>
2025-05-30 01:43:42 +0330

    feat: add store page

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,29 +1,40 @@
 import Container from "../../components/container/Container";
 import ProductItem from "../../components/productItem/ProductItem";
 import { Link } from "react-router-dom";
+import { getProducts } from "../../services/api";
+import { useEffect, useState } from "react";
+import type { Products } from "../../types/servers";
+
 
 export default function Store() {
+const [products, setProducts] = useState<Products[]>([]);
+
+
+useEffect(() => {
+  getProducts()
+    .then((res) => {
+      console.log("products loaded:", res);
+      setProducts(res);
+    })
+    .catch((err) => {
+      console.error("error loading products:", err);
+    });
+}, []);
+  
   return (
     <div>
       <Container>
         <h1 className="my-5">جدیدترین محصولات</h1>
 
         <div className="grid grid-cols-4 gap-4">
-          <Link to={`/product/${1}`}>
-            <ProductItem />
+          {products.map((item) => (
+            <Link to={`/product/${item.id}`}>
+            <ProductItem {...item}/>
           </Link>
-          <Link to={`/product/${2}`}>
-            <ProductItem />
-          </Link>
-          <Link to={`/product/${3}`}>
-            <ProductItem />
-          </Link>{" "}
-          <Link to={`/product/${4}`}>
-            <ProductItem />
-          </Link>
-          <Link to={`/product/${5}`}>
-            <ProductItem />
-          </Link>
+          ))}
+
+          
+         
         </div>
       </Container>
     </div>
